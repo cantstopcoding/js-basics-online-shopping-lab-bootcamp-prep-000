1beaf0097c8cece01f22db04051d8ee381eebaff
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-12 02:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function total() {
 function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++) {
     if(item === getCart()[i].itemName) {
-      car.splice(i, 0);
+      cart.splice(i, 0);
     }
   }
 }
