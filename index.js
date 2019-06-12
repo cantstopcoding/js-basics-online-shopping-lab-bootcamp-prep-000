ecb9be42a24765603d8690f451b9e4187f4ca260
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-12 02:18:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -74,4 +74,7 @@ function placeOrder(cardNumber) {
   if(arguments.length === 0) {
     return "Sorry, we don't have a credit card on file for you.";
   }
+  if(arguments.length === 1) {
+    
+  }
 }
