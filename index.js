bb848e5192d9e620b2891a43d9539ecfe6385454
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-12 02:27:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -76,6 +76,8 @@ function placeOrder(cardNumber) {
   }
   
   if(arguments.length === 1) {
+    cartTotal = total();
+    cart.length = 0;
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
 }
